bcfef316721e3491c9e4fc2892939d25bf085c23
TempexX <52248020+TempexX@users.noreply.github.com>
2024-05-15 21:37:36 +0200

    Hej

--- MineTimeLoen/app/navigations/Navbar.js
+++ MineTimeLoen/app/navigations/Navbar.js
@@ -6,6 +6,9 @@ import Konto from './Konto.js';
 import Overblik from './Overblik.js';
 import MineData from './MineData.js';
 import Vagter from './Vagter.js';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+
 
 const Tab = createBottomTabNavigator();
 
@@ -13,7 +16,13 @@ const Tab = createBottomTabNavigator();
 export default function navbar() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={{
+        headerShown:false,
+        tabBarIcon:({color,size})=>(
+          <FontAwesome name="tachometer" size={24} color={color} />
+        )
+
+      }} >
         <Tab.Screen name='Overblik' component={Overblik}/>
         <Tab.Screen name='Vagter' component={Vagter}/>
         <Tab.Screen name='Mine Data' component={MineData}/>
